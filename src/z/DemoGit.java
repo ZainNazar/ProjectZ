5d59941b66e708ace4f6f410619675f830ba4f18
ZainNazar <155387033+ZainNazar@users.noreply.github.com>
2024-01-05 13:57:41 -0600

    Update DemoGit.java

--- src/z/DemoGit.java
+++ src/z/DemoGit.java
@@ -7,10 +7,8 @@ public class DemoGit {
 
 	public static void main(String[] args) {
 		System.out.println("Sprint One");
-
 	}
-// Stage 2 Coming from gitHub
-
+// Stage 2 Coming from gitHub //
 	public void add(int a, int b) {
 		System.out.println(a + b);
 	}
