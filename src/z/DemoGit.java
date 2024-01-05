2a0288692c800c62612ad2d4b1bcf871a017409f
zalkh <zalkh@10.0.0.55>
2024-01-05 13:53:49 -0600

    Declare Two Variable

--- src/z/DemoGit.java
+++ src/z/DemoGit.java
@@ -2,13 +2,17 @@ package z;
 
 public class DemoGit {
 
+	int age;
+	String name;
+
 	public static void main(String[] args) {
 		System.out.println("Sprint One");
 
 	}
 // Stage 2 Coming from gitHub
 
-	public void add (int a , int b){
-		System.out.println(a+b);
+	public void add(int a, int b) {
+		System.out.println(a + b);
 	}
+
 }
